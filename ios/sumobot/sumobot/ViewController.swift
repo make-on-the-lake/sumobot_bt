8e76cc4f3606bb239505aa204b440b05aac27563
Gary Johnson <gary@gjtt.com>
2015-04-18 18:19:23 -0400

    Implement control screen on ios

--- ios/sumobot/sumobot/ViewController.swift
+++ ios/sumobot/sumobot/ViewController.swift
@@ -1,25 +1,58 @@
-//
-//  ViewController.swift
-//  sumobot
-//
-//  Created by Gary Johnson on 3/29/15.
-//  Copyright (c) 2015 SparkBarn. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftThumb: UIImageView!
+    @IBOutlet weak var rightThumb: UIImageView!
+    @IBOutlet weak var leftTrack: UIImageView!
+    @IBOutlet weak var rightTrack: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func onLeftThumbDragged(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view);
+        if let view = recognizer.view {
+            if recognizer.state == UIGestureRecognizerState.Ended {
+                UIView.animateWithDuration(
+                    0.4, delay: 0,
+                    usingSpringWithDamping: 0.3,
+                    initialSpringVelocity: 1,
+                    options: UIViewAnimationOptions.CurveEaseOut,
+                    animations: {() in view.center = self.leftTrack.center},
+                    completion: nil)
+                return
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            var newY = view.center.y + translation.y
+            if newY > leftTrack.frame.origin.y && newY < leftTrack.frame.origin.y + leftTrack.frame.size.height {
+                view.center = CGPoint(x:view.center.x, y:view.center.y + translation.y)
+                recognizer.setTranslation(CGPointZero, inView:self.view)
+            }
+        }
     }
+    
+    @IBAction func onRightThumbDragged(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view);
+        if let view = recognizer.view {
+            if recognizer.state == UIGestureRecognizerState.Ended {
+                UIView.animateWithDuration(
+                    0.4, delay: 0,
+                    usingSpringWithDamping: 0.3,
+                    initialSpringVelocity: 1,
+                    options: UIViewAnimationOptions.CurveEaseOut,
+                    animations: {() in view.center = self.rightTrack.center},
+                    completion: nil)
+                return
+            }
 
-
+            var newY = view.center.y + translation.y
+            if newY > rightTrack.frame.origin.y && newY < rightTrack.frame.origin.y + rightTrack.frame.size.height {
+                view.center = CGPoint(x:view.center.x, y:view.center.y + translation.y)
+                recognizer.setTranslation(CGPointZero, inView:self.view)
+            }
+        }
+    }
 }
 
