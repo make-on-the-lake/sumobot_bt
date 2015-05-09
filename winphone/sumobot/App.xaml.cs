98e3ce7a8d4ff18eebf41f2bfd284724f26b8c14
Gary Johnson <gary@gjtt.com>
2015-05-09 11:55:40 -0400

    Adding windows phone project

---
+++ winphone/sumobot/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace sumobot {
+
+    public sealed partial class App : Application {
+
+        private TransitionCollection transitions;
+
+        public App() {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e) {
+
+            var rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) {
+                rootFrame = new Frame();
+                rootFrame.CacheSize = 1;
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null) {
+                if (rootFrame.ContentTransitions != null) {
+                    transitions = new TransitionCollection();
+                    foreach (var c in rootFrame.ContentTransitions) {
+                        transitions.Add(c);
+                    }
+                }
+
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += RootFrame_FirstNavigated;
+
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments)) {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            Window.Current.Activate();
+        }
+
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e) {
+            var rootFrame = (Frame)sender;
+            rootFrame.ContentTransitions = transitions ?? new TransitionCollection() { new NavigationThemeTransition() };
+            rootFrame.Navigated -= RootFrame_FirstNavigated;
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e) {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
---
+++ winphone/sumobot/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace sumobot {
+
+    public sealed partial class MainPage : Page {
+
+        public MainPage() {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+            HideStatusBar();
+
+            LeftThumb.ManipulationMode = ManipulationModes.TranslateY;
+            RightThumb.ManipulationMode = ManipulationModes.TranslateY;
+            LeftThumb.ManipulationDelta += OnThumbManipulationDelta;
+            RightThumb.ManipulationDelta += OnThumbManipulationDelta;
+            LeftThumb.ManipulationCompleted += OnThumbManipulationCompleted;
+            RightThumb.ManipulationCompleted += OnThumbManipulationCompleted;
+        }
+
+        private void OnThumbManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs args) {
+            var thumb = (FrameworkElement) sender;
+            var translate = (TranslateTransform) thumb.RenderTransform;
+            var yTranslation = Math.Max(args.Cumulative.Translation.Y, -LeftTrack.ActualHeight/2);
+            yTranslation = Math.Min(yTranslation, LeftTrack.ActualHeight/2);
+            translate.Y = yTranslation;
+        }
+
+        private void OnThumbManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs args) {
+            var thumb = (FrameworkElement) sender;
+
+            var animation = new DoubleAnimation();
+            var ease = new ElasticEase();
+            ease.Oscillations = 1;
+            ease.Springiness = 1;
+            animation.EasingFunction = ease;
+            animation.To = 0;
+            animation.Duration = new Duration(new TimeSpan(0,0,0,0,175));
+            Storyboard.SetTarget(animation, thumb.RenderTransform);
+            Storyboard.SetTargetProperty(animation, "Y");
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        public async void HideStatusBar() {
+            await Windows.UI.ViewManagement.StatusBar.GetForCurrentView().HideAsync();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs args) {
+        }
+    }
+}
