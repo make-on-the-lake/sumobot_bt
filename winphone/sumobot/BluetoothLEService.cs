392004e4b3febef368be6389ce4e2ab8b74124b3
Gary Johnson <gary@gjtt.com>
2015-05-10 23:07:06 -0400

    Windows Phone project working for drive commands

---
+++ winphone/sumobot/BluetoothLEService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+
+namespace sumobot {
+
+    public class BluetoothLEService {
+
+        public BluetoothLEService(ushort serviceId, ushort writeId) {
+            _serviceId = serviceId;
+            _writeId = writeId;
+        }
+
+        private readonly ushort _serviceId;
+        private readonly ushort _writeId;
+        private GattDeviceService _connectedService;
+
+        public event EventHandler Connected;
+        public event EventHandler Disconnected;
+
+        public string Name { get; set; }
+        public CoreDispatcher Dispatcher { get; set; }
+
+        public async Task StartSearching() {
+            try {
+                var deviceSelector = GattDeviceService.GetDeviceSelectorFromShortId(_serviceId);
+                var devices = await DeviceInformation.FindAllAsync(deviceSelector);
+                foreach (var device in devices) {
+                    if (String.Equals(device.Name, Name, StringComparison.CurrentCultureIgnoreCase)) {
+                        _connectedService = await GattDeviceService.FromIdAsync(device.Id);
+                        if (_connectedService != null) {
+                            _connectedService.Device.ConnectionStatusChanged += OnServiceConnectionStatusChanged;
+                            OnConnected();
+                            return;
+                        }
+                    }
+                }
+
+                await StartSearching();
+            }
+            catch (Exception ex) {
+
+                throw ex;
+            }
+        }
+
+        private void OnServiceConnectionStatusChanged(BluetoothLEDevice sender, object args) {
+            if (sender.ConnectionStatus == BluetoothConnectionStatus.Disconnected) {
+                Stop();
+                StartSearching();
+            }
+        }
+
+        public void Stop() {
+            if(_connectedService != null)
+                _connectedService.Device.ConnectionStatusChanged -= OnServiceConnectionStatusChanged;
+            _connectedService = null;
+            OnDisconnected();
+        }
+
+        public async void Write(byte[] data) {
+            if (!IsConnected)
+                return;
+
+            var writeCharacterisic = _connectedService.GetCharacteristics(GattCharacteristic.ConvertShortIdToUuid(_writeId))[0];
+            var dataWriter = new DataWriter();
+            dataWriter.WriteBytes(data);
+            await writeCharacterisic.WriteValueAsync(dataWriter.DetachBuffer(), GattWriteOption.WriteWithoutResponse);
+        }
+
+        public bool IsConnected {
+            get { return _connectedService != null; }
+        }
+
+        protected virtual void OnConnected() {
+            var handler = Connected;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnDisconnected() {
+            var handler = Disconnected;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+    }
+}
--- winphone/sumobot/MainPage.xaml.cs
+++ winphone/sumobot/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -9,38 +11,89 @@ using Windows.UI.Xaml.Navigation;
 namespace sumobot {
 
     public sealed partial class MainPage : Page {
+        private readonly byte[] START = { 0xAB };
+        private readonly byte[] MOTOR_ID = { 0x01 };
+        private readonly byte[] BUTTON_ID = { 0x02 };
+        private readonly byte[] MOTOR_PADDING = { 0x00, 0x00, 0x00, 0x00 };
+        private readonly byte[] BUTTON_PADDING = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        private readonly byte[] EMPTY = { 0x00 };
+        private readonly byte[] END = { 0xEF };
+        private const int TRANSMIT_INTERVAL_MSEC = 10;
+        private readonly BluetoothLEService _bluetoothService;
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
 
         public MainPage() {
             InitializeComponent();
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, TRANSMIT_INTERVAL_MSEC);
+            _timer.Tick += OnWriteTimerTick;
+            _bluetoothService = new BluetoothLEService(0xffe0, 0xffe1);
+            _bluetoothService.Dispatcher = Dispatcher;
+            _bluetoothService.Connected += OnBluetoothConnected;
+            _bluetoothService.Disconnected += OnBluetoothDisconnected;
             NavigationCacheMode = NavigationCacheMode.Required;
             HideStatusBar();
+            OnShowSettings();
+            Loaded += OnLoaded;
+        }
 
-            LeftThumb.ManipulationMode = ManipulationModes.TranslateY;
-            RightThumb.ManipulationMode = ManipulationModes.TranslateY;
-            LeftThumb.ManipulationDelta += OnThumbManipulationDelta;
-            RightThumb.ManipulationDelta += OnThumbManipulationDelta;
-            LeftThumb.ManipulationCompleted += OnThumbManipulationCompleted;
-            RightThumb.ManipulationCompleted += OnThumbManipulationCompleted;
+        private void OnBluetoothConnected(object sender, EventArgs e) {
+            StatusTextBlock.Text = "CONNECTED";
+            ProgressRing.Opacity = 0d;
+            _timer.Start();
+        }
+
+        private void OnWriteTimerTick(object sender, object e) {
+            if(_bluetoothService.IsConnected)
+                _bluetoothService.Write(BuildCommandMessage());
+        }
+
+        private void OnBluetoothDisconnected(object sender, EventArgs e) {
+            _timer.Stop();
+            StatusTextBlock.Text = "SEARCHING";
+            ProgressRing.Opacity = 1d;
+        }
+
+        public void OnLoaded(object sender, RoutedEventArgs e) {
+            NameTextBox.Focus(FocusState.Keyboard);
+        }
+
+        private void OnShowSettings() {
+            _bluetoothService.Stop();
+            SettingsButton.Visibility = Visibility.Collapsed;
+            BackButton.Visibility = Visibility.Visible;
+            StatusTextBlock.Opacity = 0d;
+            NameTextBox.IsReadOnly = false;
+            ProgressRing.Opacity = 0d;
+            NameTextBox.Focus(FocusState.Keyboard);
+        }
+
+        private void OnLeavingSettings() {
+            StatusTextBlock.Text = "SEARCHING";
+            ProgressRing.Opacity = 1d;
+            SettingsButton.Visibility = Visibility.Visible;
+            BackButton.Visibility = Visibility.Collapsed;
+            StatusTextBlock.Opacity = 1d;
+            NameTextBox.IsReadOnly = true;
+            NameTextBox.IsEnabled = false;
+            NameTextBox.IsEnabled = true;
+            _bluetoothService.Name = NameTextBox.Text;
+            _bluetoothService.StartSearching();
         }
 
         private void OnThumbManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs args) {
-            var thumb = (FrameworkElement) sender;
-            var translate = (TranslateTransform) thumb.RenderTransform;
-            var yTranslation = Math.Max(args.Cumulative.Translation.Y, -LeftTrack.ActualHeight/2);
-            yTranslation = Math.Min(yTranslation, LeftTrack.ActualHeight/2);
+            var thumb = (FrameworkElement)sender;
+            var translate = (TranslateTransform)thumb.RenderTransform;
+            var yTranslation = Math.Max(args.Cumulative.Translation.Y, -LeftTrack.ActualHeight / 2);
+            yTranslation = Math.Min(yTranslation, LeftTrack.ActualHeight / 2);
             translate.Y = yTranslation;
         }
 
         private void OnThumbManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs args) {
-            var thumb = (FrameworkElement) sender;
+            var thumb = (FrameworkElement)sender;
 
-            var animation = new DoubleAnimation();
-            var ease = new ElasticEase();
-            ease.Oscillations = 1;
-            ease.Springiness = 1;
-            animation.EasingFunction = ease;
-            animation.To = 0;
-            animation.Duration = new Duration(new TimeSpan(0,0,0,0,175));
+            var ease = new ElasticEase { Oscillations = 1, Springiness = 1 };
+            var animation = new DoubleAnimation { EasingFunction = ease, To = 0 };
+            animation.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 175));
             Storyboard.SetTarget(animation, thumb.RenderTransform);
             Storyboard.SetTargetProperty(animation, "Y");
             var storyboard = new Storyboard();
@@ -49,10 +102,49 @@ namespace sumobot {
         }
 
         public async void HideStatusBar() {
-            await Windows.UI.ViewManagement.StatusBar.GetForCurrentView().HideAsync();
+            await StatusBar.GetForCurrentView().HideAsync();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs args) {
+        private byte[] BuildCommandMessage() {
+            var command = new List<byte>();
+            command.AddRange(START);
+            command.AddRange(MOTOR_ID);
+            command.AddRange(BitConverter.GetBytes(GetLeftSpeed()));
+            command.AddRange(BitConverter.GetBytes(GetRightSpeed()));
+            command.AddRange(MOTOR_PADDING);
+            command.AddRange(EMPTY);
+            command.AddRange(END);
+            return command.ToArray();
+        }
+
+        private UInt32 GetLeftSpeed() {
+            var translate = (TranslateTransform)LeftThumb.RenderTransform;
+            double yRatio = translate.Y / (LeftTrack.ActualHeight / 2d);
+            double speed = (yRatio * 512d) + 512d;
+            speed = Math.Min(speed, 1024d);
+            speed = Math.Max(speed, 0d);
+            return (UInt32)speed;
+        }
+
+        private UInt32 GetRightSpeed() {
+            var translate = (TranslateTransform)RightThumb.RenderTransform;
+            double yRatio = translate.Y / (RightTrack.ActualHeight / 2d);
+            double speed = (yRatio * 512d) + 512d;
+            speed = Math.Min(speed, 1024d);
+            speed = Math.Max(speed, 0d);
+            return (UInt32)speed;
+        }
+
+        private void OnSettingsTapped(object sender, RoutedEventArgs e) {
+            OnShowSettings();
+        }
+
+        private void OnBackTapped(object sender, RoutedEventArgs e) {
+            OnLeavingSettings();
+        }
+
+        private void OnTextLostFocus(object sender, RoutedEventArgs e) {
+            NameTextBox.Text = NameTextBox.Text.ToUpper();
         }
     }
 }
